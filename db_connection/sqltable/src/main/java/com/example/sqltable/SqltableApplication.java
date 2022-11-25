b20a4a1f3b85b36a5b057a3b9692064d269d46f6
Sandhyamagesh <113769406+Sandhyamagesh@users.noreply.github.com>
2022-11-25 23:42:53 +0530

    Add files via upload

---
+++ db_connection/sqltable/src/main/java/com/example/sqltable/SqltableApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqltable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqltableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqltableApplication.class, args);
+	}
+
+}
---
+++ db_connection/sqltable/src/test/java/com/example/sqltable/SqltableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqltable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqltableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
