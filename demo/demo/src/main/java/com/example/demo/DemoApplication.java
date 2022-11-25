cadbf337a52a4a0a985578144bab5e1d028fef32
Sandhyamagesh <113769406+Sandhyamagesh@users.noreply.github.com>
2022-11-25 23:38:59 +0530

    Add files via upload

---
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner{
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		String sql = "INSERT INTO employees (id, first_name, last_name, email) VALUES (?, ?, ?, ?)";
+		int result = jdbcTemplate.update(sql, 200, "Ravi", "Rahul", "ravi@gmail.com");
+		if( result>0){
+			System.out.println("A new row has been created");
+		}
+	}
+
+}
